65d966abfc83928a184cf10ebfc5ac341d64b60e
ttvone-macbook <ttvone-macbook@10.50.41.200>
2015-09-12 22:29:28 +0700

    change valiable to constant PATH_ROOT

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 
+define("PATH_ROOT", dirname(__FILE__));
 require_once 'autoload.php';
 if (isset($_SERVER['PATH_INFO'])) {
     $path_info = explode('/', $_SERVER['PATH_INFO']);
     $controller_info = strtolower($path_info[1]);
     $controller_path = 'controllers/' . $controller_info . 'Controller.php';
     if (file_exists($controller_path)) {
-        $controller_path = dirname(__FILE__) . '/' . $controller_path;
+        $controller_path = PATH_ROOT . '/' . $controller_path;
         require_once ($controller_path);
         eval('new ' . $controller_info . 'Controller();');
         unset($path_info, $controller_info, $controller_path);
